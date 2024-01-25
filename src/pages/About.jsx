fea5fde9503bb9d315c2c594082909da2ae32bc5
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2024-01-25 12:27:05 +0100

    remplire home et about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Card, Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our car showroom! Here are some of the amazing cars we have
+        in stock:
+      </p>
+
+      <Row xs={1} md={2} lg={3} className="g-4">
+        {storeItems.map((car) => (
+          <Col key={car.id}>
+            <Card>
+              <Card.Img variant="top" src={car.imgUrl} />
+              <Card.Body>
+                <Card.Title>{car.name}</Card.Title>
+                <Card.Text>Price: ${car.price.toLocaleString()}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json";
+import { Row, Col, Form } from "react-bootstrap";
 
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedBrand, setSelectedBrand] = useState("All");
+  const [filteredData, setFilteredData] = useState(storeItems);
+
+  const handleSearch = (e) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+    filterItems(term, selectedBrand);
+  };
+
+  const handleBrandChange = (e) => {
+    const brand = e.target.value;
+    setSelectedBrand(brand);
+    filterItems(searchTerm, brand);
+  };
+
+  const filterItems = (term, brand) => {
+    const filteredItems = storeItems.filter(
+      (item) =>
+        item.name.toLowerCase().includes(term) &&
+        (brand === "All" ||
+          item.name.toLowerCase().includes(brand.toLowerCase()))
+    );
+    setFilteredData(filteredItems);
+  };
+
+  const brands = [
+    "All",
+    "BMW",
+    "MERCEDES AMG",
+    "LAMBORGHINI",
+    "MCLAREN",
+    "PORSCHE",
+  ];
+
   return (
-    <div>
+    <>
       <h1>Home</h1>
-    </div>
+
+      <Form className="mb-3">
+        <Form.Group controlId="searchBar">
+          <Form.Control
+            type="text"
+            placeholder="entre votre marque"
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </Form.Group>
+        <Form.Group controlId="brandFilter">
+          <Form.Select
+            value={selectedBrand}
+            onChange={handleBrandChange}
+            aria-label="Car Brands"
+          >
+            {brands.map((brand) => (
+              <option key={brand} value={brand}>
+                {brand}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Form>
+
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {filteredData.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
